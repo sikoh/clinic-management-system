3cbde668b42f232197ecae9c1793afac647bdfa0
Silvana K <84741857+sikoh@users.noreply.github.com>
2022-07-11 09:07:44 -0400

    create endpoint to delete a doctor

---
+++ endpoints/doctor_delete.py
@@ -0,0 +1,14 @@
+from db import *
+
+from models.doctors import Doctors
+
+def doctor_update(doctor_id):
+  doctor_record = db.session.query(Doctors).filter(Doctors.id == doctor_id).first()
+
+  if doctor_record:
+      db.session.delete(doctor_record)
+      db.session.commit()
+      return jsonify(f'Doctor with doctor_id {doctor_id} deleted'), 200
+
+
+  return (f'Doctor with doctor_id {doctor_id} not found'), 404
